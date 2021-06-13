ff1948907255b21f9b8093bf9c54f9089a3d50d8
zephyrus <pandey.piyus@gmail.com>
2021-06-13 16:12:53 +0530

    adding socket.io utils

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const port = 3000;
 const publicDir = path.join(__dirname, '../public');
 
 app.use(express.static(publicDir));
 
-// app.get('/', (req, res) => {
-//   res.send();
-// });
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Hello at:', port);
 });
